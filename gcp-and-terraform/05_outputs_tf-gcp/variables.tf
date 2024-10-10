45798885798361e219877cd368d15c5cbdd0f1ab
Tcarters <cartersedmund@gmail.com>
2024-10-10 01:38:49 +0000

    gcp solutions and restructured folder

---
+++ gcp-and-terraform/05_outputs_tf-gcp/variables.tf
@@ -0,0 +1,26 @@
+variable "project" {
+  
+}
+
+variable "credentials_file" { 
+
+}
+
+
+variable "region" {
+  default = "us-east1"
+}
+
+variable "zone" {
+  default = "us-east1-a"
+}
+
+variable "runtime" {
+  default =  "nodejs20"
+
+}
+
+variable "topic" {
+  default = "cf-demo"
+  
+}
